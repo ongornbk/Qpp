4a51f97361722fe0d1cca372f52390b2a700eb3d
Irish <ongornbk@gmail.com>
2018-09-13 14:44:29 +0200

    v0.02d

--- R11/Math.cpp
+++ R11/Math.cpp
@@ -4,6 +4,22 @@
 #include <math.h>
 #include <chrono>
 
+struct COLOR
+{
+	union
+	{
+		struct
+		{
+			uint8_t r;
+			uint8_t g;
+			uint8_t b;
+		};
+		uint32_t color;
+
+	};
+
+};
+
 namespace
 {
 	static lua_State* m_lua = nullptr;
@@ -29,10 +45,21 @@ static int32_t _cdecl IntegerLength(lua_State* state)
 		return 1;
 }
 
+static int32_t _cdecl ToColor(lua_State* state)
+{
+	COLOR rgb;
+	rgb.r = lua_tointeger(state, 1);
+	rgb.g = lua_tointeger(state, 2);
+	rgb.b = lua_tointeger(state, 3);
+	lua_pushinteger(state, rgb.color);
+	return 1;
+}
+
 void _stdcall MathPackageInitializer()
 {
 	srand(time(NULL));
 	m_lua = LuaManager::GetInstance()->m_lua;
 	lua_register(m_lua, "RandomInteger", RandomInteger);
 	lua_register(m_lua, "IntegerLength", IntegerLength);
+	lua_register(m_lua, "ToColor", ToColor);
 }
